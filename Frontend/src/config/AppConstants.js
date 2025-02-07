bfd64024b6eff35ff3a5ee26fe383e9670c69d87
Mugeshwaran <mugeshwaranthedeveloper@gmail.com>
2025-02-07 16:41:39 +0530

    Remove unused logo image and update AppConstants; comment out login token storage in Login component

--- Frontend/src/config/AppConstants.js
+++ Frontend/src/config/AppConstants.js
@@ -1,6 +1,6 @@
 const APP_CONSTANTS = {
   App_Name: "Nexemy",
-  App_Logo: "/assets/img/logo/nexemy.png",
+  App_Logo: "/assets/img/logo/logo.png",
   Favicon: "/assets/img/logo/favicon.png",
   profile_img: "/assets/img/default/avatar-default.png",
   demo_video: "/assets/img/default/avatar-default.png",
