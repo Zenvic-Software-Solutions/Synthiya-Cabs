6a0e4adea88902f4048e503c94852aaff9c2b291
Vishva <vishva01mail@gmail.com>
2025-02-08 14:55:46 +0530

    Refactor Bank serializers to use Meta class for field definitions

--- Backend/apps/CMS/serializers/bank.py
+++ Backend/apps/CMS/serializers/bank.py
@@ -2,27 +2,29 @@ from apps.CMS.models import Bank, BankBalance
 from apps.BASE.serializers import ReadSerializer, WriteSerializer
 
 class BankReadSerializer(ReadSerializer):
-    model=Bank
-    fields = [
-        "id",
-        "uuid",
-        "identity",
-        "branch",
-        "account_no",
-        "account_holder_name",
-        "ifsc_code",
-    ]
+    class Meta(ReadSerializer.Meta):
+        model=Bank
+        fields = [
+            "id",
+            "uuid",
+            "identity",
+            "branch",
+            "account_no",
+            "account_holder_name",
+            "ifsc_code",
+        ]
 
 
 class BankWriteSerializer(WriteSerializer):
-    model=Bank
-    fields = [
-        "identity",
-        "branch",
-        "account_no",
-        "account_holder_name",
-        "ifsc_code",
-    ]
+    class Meta(WriteSerializer.Meta):
+        model=Bank
+        fields = [
+            "identity",
+            "branch",
+            "account_no",
+            "account_holder_name",
+            "ifsc_code",
+        ]
 
 
 class BankBalanceReadSerializer(ReadSerializer):
