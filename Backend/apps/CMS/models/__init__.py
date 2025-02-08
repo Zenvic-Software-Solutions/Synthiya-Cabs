da90b6e4201206947f39ff01c4aaaa0d26a815c8
Vishva <vishva01mail@gmail.com>
2025-02-08 17:01:43 +0530

    Refactor model imports for consistency and clarity; update ForeignKey references in Betta, Booking, and Payment models

--- Backend/apps/CMS/models/__init__.py
+++ Backend/apps/CMS/models/__init__.py
@@ -1,8 +1,8 @@
+from .branch import Branch
 from .other_cab import OtherCab,OtherDriver,OtherVehicle
 from .vechile import Vechile
 from .bank import Bank ,BankBalance
 from .workshop import Workshop ,Maintenance
-from .betta import Betta
 from .booking import Booking
-from .branch import Branch
+from .betta import Betta
 from .payment import Payment
--- Backend/apps/CMS/models/betta.py
+++ Backend/apps/CMS/models/betta.py
@@ -1,11 +1,10 @@
 from apps.BASE.models import DEFAULT_BLANK_NULLABLE_FIELD_CONFIG, MAX_CHAR_FIELD_LENGTH, BaseModel
 from django.db import models
 from apps.ACCESS.models import Driver
-from apps.CMS.models import booking
 
 class Betta(BaseModel):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name="bettas")
-    booking = models.ForeignKey(booking, on_delete=models.CASCADE, related_name="bettas")
+    booking = models.ForeignKey("CMS.Booking", on_delete=models.CASCADE, related_name="bettas")
     
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     
