5216ad63a5f83df8f9836c0592842ffab02ef148
PSGSudalai <sudalaimuthug397@gmail.com>
2025-02-10 16:49:28 +0530

    driver

--- Backend/apps/CMS/models/other_cab.py
+++ Backend/apps/CMS/models/other_cab.py
@@ -3,26 +3,25 @@ from apps.BASE.models import DEFAULT_BLANK_NULLABLE_FIELD_CONFIG, MAX_CHAR_FIELD
 from django.db import models
 from HELPERS.choices import VEHICLE_TYPE
 
-class OtherDriver(BaseModel):
-    identity = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
-    # other_cab_name =models.ForeignKey(OtherCab,on_delete=models.CASCADE,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
-    phone_number = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
 
-    def __str__(self):
-        return f"{self.identity} {self.other_cab_name.identity}"
-    
 class OtherCab(BaseModel):
     identity = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
     owner_name = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
     phone_number = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
     address = models.TextField(**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
     balance = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
-    otherdriver = models.ForeignKey(OtherDriver,on_delete=models.CASCADE,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
-
+    
     def __str__(self):
         return f"{self.identity} {self.owner_name}"
     
+class OtherDriver(BaseModel):
+    identity = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
+    other_cab_name =models.ForeignKey(OtherCab,on_delete=models.CASCADE,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
+    phone_number = models.CharField(max_length=MAX_CHAR_FIELD_LENGTH,**DEFAULT_BLANK_NULLABLE_FIELD_CONFIG)
 
+    def __str__(self):
+        return f"{self.identity} {self.other_cab_name.identity}"
+    
 
 
 class OtherVehicle(BaseModel):
