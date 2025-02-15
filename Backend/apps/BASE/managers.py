110efc5679485dacb6430bfda4539a7eec5f56fe
Vishva <vishva01mail@gmail.com>
2025-02-15 13:17:49 +0530

    Refactor UserManager to uncomment and enforce is_staff checks for superuser creation

--- Backend/apps/BASE/managers.py
+++ Backend/apps/BASE/managers.py
@@ -25,16 +25,16 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, phone_number, password=None, **extra_fields):
-        # extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(phone_number, password, **extra_fields)
 
     def create_superuser(self, phone_number, password=None, **extra_fields):
-        # extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
-        # if extra_fields.get("is_staff") is not True:
-        #     raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
